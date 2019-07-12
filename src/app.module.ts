fa0480669a69fdd789ae136d7bfbe48180269a36
annassdan <annassdan@gmail.com>
2019-07-12 14:10:27 +0700

    update appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BankModule } from './ideas/bank/bank.module';
 import { TransactionModule } from './ideas/transaction/transaction.module';
 import { WarehouseModule } from './ideas/warehouse/warehouse.module';
+import { ItemModule } from './ideas/item/item.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     BankModule,
     TransactionModule,
-    WarehouseModule
+    WarehouseModule,
+    ItemModule
   ],
   controllers: [AppController],
   providers: [AppService],
