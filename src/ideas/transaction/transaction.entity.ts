9f8b0230196e4afcb14e7721f51e3affee363d63
annassdan <annassdan@gmail.com>
2019-07-12 14:01:56 +0700

    remove unused import

--- src/ideas/transaction/transaction.entity.ts
+++ src/ideas/transaction/transaction.entity.ts
@@ -1,6 +1,5 @@
 import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { TransactionType } from '../../utils/enums/transaction.type';
-import { Bank } from '../bank/bank.entity';
 import { ApiModelProperty } from '@nestjs/swagger';
 import { Warehouse } from '../warehouse/warehouse.entity';
 
